eee7edb37bb631d881ebc203d98a571f9e3dd06b
Imrich Zeleňák <92807683+Luminarko@users.noreply.github.com>
2022-04-11 20:40:47 +0200

    Add files via upload

---
+++ php-sql/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Flights</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Flight Code</th>
+            <th>Start</th>
+            <th>Start Airport</th>
+            <th>End</th>
+            <th>End Airport</th>
+            <th>Gate</th>
+        </tr>
+        <?php 
+        include "dat/data.php";
+        ?>
+    </table>
+</body>
+</html>
